9f37751d14a7c2ad4827d348b41d080dfa094242
Adrian <adrianpatycki@gmail.com>
2022-02-09 12:34:08 +0100

    P2022-702 Adds validation of login and password fields

--- app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
@@ -2,10 +2,42 @@ package com.intive.patronage22.intivi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton: Button = findViewById(R.id.loginButton)
+        val loginEditText: EditText = findViewById(R.id.loginEditText)
+        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
+
+        loginButton.setOnClickListener {
+
+            fun loginValidation(): Boolean {
+                val isLoginOK: Boolean
+                val isPasswordOK: Boolean
+
+                if (loginEditText.text.isEmpty()) {
+                    loginEditText.error = ""
+                    isLoginOK = false
+                } else {
+                    isLoginOK = true
+                }
+
+                if (passwordEditText.text.length < 5) {
+                    passwordEditText.error = ""
+                    isPasswordOK = false
+                } else {
+                    isPasswordOK = true
+                }
+
+                return isLoginOK && isPasswordOK
+            }
+
+            loginValidation()
+        }
     }
 }
