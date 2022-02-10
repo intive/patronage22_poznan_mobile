3a35a3ff22f6f84f7df996c0d13a4a3112236d6d
unknown <marta.pienkowska@gmail.com>
2022-02-10 20:51:17 +0100

    adds navigation to home screen

--- app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
@@ -7,5 +7,15 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<Button>(R.id.loginButton).setOnClickListener(listener)
     }
+    private val listener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.loginButton -> {
+                startActivity( Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+
 }
