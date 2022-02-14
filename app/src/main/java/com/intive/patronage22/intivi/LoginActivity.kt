267f2fabdc2a37466cf1a86376ca2a0e75e04a87
PienkowskaMarta <38153748+PienkowskaMarta@users.noreply.github.com>
2022-02-14 17:06:06 +0100

    P2022 703 (#9)
    * adds navigation to home screen
    
    * adds missing imports

--- app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.intive.patronage22.intivi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewById<Button>(R.id.loginButton).setOnClickListener(listener)
     }
+    private val listener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.loginButton -> {
+                startActivity( Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+
 }
