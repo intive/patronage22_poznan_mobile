76f97802bdce5d63259e837b5378be3eadafc246
Adrian Patycki <97976278+AdrianPat@users.noreply.github.com>
2022-02-14 19:47:40 +0100

    P2022-702 Login form validation (#6)
    * P2022-702 Adds validation of login and password fields
    
    * P2022-702 Corrects login form validation:
    
    - removes useless values
    - adds variable (isLoginFormValid) for later use to navigation
    - separate functions validates login and password fields
    
    * P2022-702 Puts "findViewById<EditText>(R.id.loginEditText)" into "val editText"
    
    Co-authored-by: Lucas-Fresko <98414007+Lucas-Fresko@users.noreply.github.com>

--- app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/LoginActivity.kt
@@ -3,24 +3,51 @@ package com.intive.patronage22.intivi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-
-
+import android.widget.EditText
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
+
+    private var isLoginFormValid: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        findViewById<Button>(R.id.loginButton).setOnClickListener(listener)
-    }
-    private val listener = View.OnClickListener { view ->
-        when (view.id) {
-            R.id.loginButton -> {
-                startActivity( Intent(this, MainActivity::class.java))
-                finish()
+
+
+        fun isLoginOK(): Boolean {
+            val editText = findViewById<EditText>(R.id.loginEditText)
+            if (editText.text.isEmpty()) {
+                editText.error = ""
+                return false
+            } else {
+                return true
             }
         }
-    }
 
+        fun isPasswordOK(): Boolean {
+            val editText = findViewById<EditText>(R.id.passwordEditText)
+            if (editText.text.length < 5) {
+                editText.error = ""
+                return false
+            } else {
+                return true
+            }
+        }
+
+        val listener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.loginButton -> {
+                    isLoginFormValid = isLoginOK() and isPasswordOK()
+                    if (isLoginFormValid) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                }
+            }
+        }
+        findViewById<Button>(R.id.loginButton).setOnClickListener(listener)
+    }
 }
