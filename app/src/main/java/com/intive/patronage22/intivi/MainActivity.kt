b3db102c1f13ea72382443e01e17d0c111787982
Lucas-Fresko <98414007+Lucas-Fresko@users.noreply.github.com>
2022-02-22 14:56:23 +0100

    Remove empty lines and optimizes imports.

--- app/src/main/java/com/intive/patronage22/intivi/MainActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.intive.patronage22.intivi
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         clickNavigate(findViewById<ImageView>(R.id.imageView), DetailsActivity::class.java)
-
     }
 
     private fun clickNavigate(view: View, activity: Class<*>){
