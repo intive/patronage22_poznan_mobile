d60eed0012232b673ac002b50a8fe79e8ad5fdcf
Lucas-Fresko <98414007+Lucas-Fresko@users.noreply.github.com>
2022-02-24 14:53:52 +0100

    Changes app logo. Adds app name. Adds 'inter' font. Defines text appearance style.

--- app/src/main/java/com/intive/patronage22/intivi/MainActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        clickNavigate(findViewById<ImageView>(R.id.imageView), DetailsActivity::class.java)
+        clickNavigate(findViewById<ImageView>(R.id.app_logo), DetailsActivity::class.java)
     }
 
     private fun clickNavigate(view: View, activity: Class<*>){
