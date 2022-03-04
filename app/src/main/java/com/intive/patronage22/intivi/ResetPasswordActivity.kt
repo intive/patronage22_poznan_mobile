b18f61a4fcb4e3dc180dd09aa062a68ae43a49c9
Adrian <adrianpatycki@gmail.com>
2022-03-04 20:59:48 +0100

    P2022-1322 [Reset Password] / Show/hide password:
    - adds font "inter_medium"
    - adds 1 drawable file
    - adds functionality of show/hide password

--- app/src/main/java/com/intive/patronage22/intivi/ResetPasswordActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/ResetPasswordActivity.kt
@@ -1,10 +1,13 @@
 package com.intive.patronage22.intivi
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 
 class ResetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +18,19 @@ class ResetPasswordActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        /*
-        val showPassword = findViewById<ImageView>(R.id.imageViewShowPassword)
+        val eye = findViewById<ImageView>(R.id.imageViewShowPassword)
         val editTextResetPassword = findViewById<EditText>(R.id.editTextResetPassword)
+        val show: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.ic_show_password, null)
+        val hide: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.ic_hide_password, null)
 
-        showPassword.setOnClickListener {
-            editTextResetPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+        eye.setOnClickListener {
+            if (eye.drawable.equals(show)) {
+                editTextResetPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                eye.setImageDrawable(hide)
+            } else {
+                editTextResetPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                eye.setImageDrawable(show)
+            }
         }
-
-         */
     }
 }
