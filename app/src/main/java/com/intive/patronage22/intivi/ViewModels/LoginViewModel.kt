ff8e2af14329d89e5c8aa00bb0351a56cff18f1d
Lucas-Fresko <lucas.fresko@gmail.com>
2022-03-13 13:46:13 +0100

    Login ViewModel stub.

---
+++ app/src/main/java/com/intive/patronage22/intivi/ViewModels/LoginViewModel.kt
@@ -0,0 +1,11 @@
+package com.intive.patronage22.intivi.ViewModels
+
+import androidx.lifecycle.ViewModel
+
+
+
+
+class LoginViewModel : ViewModel(){
+    var emailHolder = ""
+    var passwordHolder = ""
+}
