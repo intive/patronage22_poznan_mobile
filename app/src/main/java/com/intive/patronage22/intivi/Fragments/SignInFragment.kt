9b8c451351ea3505c230ab883dbc447eed4aacd7
maciej.beimcik <maciej.beimcik@intive.com>
2022-03-17 19:31:56 +0100

    fixes following rebase

--- app/src/main/java/com/intive/patronage22/intivi/Fragments/SignInFragment.kt
+++ app/src/main/java/com/intive/patronage22/intivi/Fragments/SignInFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.fragment.app.Fragment
 import com.intive.patronage22.intivi.MainActivity
 import com.intive.patronage22.intivi.R
 import com.intive.patronage22.intivi.databinding.FragmentSignInBinding
