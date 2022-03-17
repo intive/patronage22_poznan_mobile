89c9aba3d88eb92afaaa691983ed69f9389d7728
Adrian <adrianpatycki@gmail.com>
2022-03-17 17:52:00 +0100

    P2022-1423 Clicking on "Forgot password"

--- app/src/main/java/com/intive/patronage22/intivi/Fragments/SignInFragment.kt
+++ app/src/main/java/com/intive/patronage22/intivi/Fragments/SignInFragment.kt
@@ -2,6 +2,9 @@ package com.intive.patronage22.intivi.Fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +21,7 @@ class SignInFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         bind = FragmentSignInBinding.inflate(layoutInflater)
 
         bind.signInButton.setOnClickListener {
