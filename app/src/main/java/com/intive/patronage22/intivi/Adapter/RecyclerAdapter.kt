0e969664f27c6955cebd2d4dc5d76058d62fc725
Bombozuar666 <bombozaur6@gmail.com>
2022-03-20 15:53:36 +0100

    P2022-1796  Last items on the grid are partially obscured repair last grid item rename RecyclerAdapter configure item_grid_home.xml

--- app/src/main/java/com/intive/patronage22/intivi/Adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/intive/patronage22/intivi/Adapter/RecyclerAdapter.kt
@@ -9,17 +9,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.intive.patronage22.intivi.R
 
 
-class RecyclerAdapater:RecyclerView.Adapter<RecyclerAdapater.ViewHolder>() {
+class RecyclerAdapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     private var titles = arrayOf("Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure","Moonfal","Sing 2","The Expanse","Death on the Nile","Little Nicholas' Treasure")
     private var images = intArrayOf(R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure,R.drawable.avatar_moonfall, R.drawable.avatar_sing_2,R.drawable.avatar_the_expanse,R.drawable.avatar_death_on_the_nile,R.drawable.avatar_little_nicholas_treasure)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapater.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.item_grid_home, parent,false)
         return ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: RecyclerAdapater.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
         holder.itemTitle.text = titles[position]
         holder.itemImage.setImageResource(images[position])
     }
