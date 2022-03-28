9b3e95afbc7b03d7963c171d950ad0401ad86220
Anna-Surma <anna.surma2000@gmail.com>
2022-03-28 11:01:38 +0200

    P2022-1861 VideoPlayerScreen/Implement layout - bug Fixed icon color changes when switching to dark mode

--- app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
@@ -3,14 +3,17 @@ package com.intive.patronage22.intivi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
 
 class VideoPlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        getApplication().setTheme(R.style.LightTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_player)
-        
+
         findViewById<ImageView>(R.id.close_video).setOnClickListener{
             finish()
         }
+
     }
 }
