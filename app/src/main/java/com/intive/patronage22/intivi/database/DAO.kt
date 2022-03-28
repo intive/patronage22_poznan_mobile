f557fadcbc2b5259110736968b096787de54f9be
Lucas-Fresko <lucas.fresko@gmail.com>
2022-03-28 13:44:48 +0200

    P2022-1323/Repository function overloading

--- app/src/main/java/com/intive/patronage22/intivi/database/DAO.kt
+++ app/src/main/java/com/intive/patronage22/intivi/database/DAO.kt
@@ -8,15 +8,21 @@ interface UserDao {
     @Query("SELECT * FROM User")
     suspend fun getUsersList(): List<User>
 
-    @Query("SELECT * FROM User WHERE username LIKE :username AND " +
+    @Query("SELECT * FROM User WHERE email LIKE :email AND " +
             "password LIKE :password LIMIT 1")
-    suspend fun findUser(username: String, password: String): User
+    suspend fun findUser(email: String, password: String): User
 
     @Query("SELECT * FROM User WHERE uid =:userID")
     suspend fun findUser(userID: Int): User
 
     @Query("SELECT * FROM User WHERE email =:vEmail")
     suspend fun findUser(vEmail: String): User
+
+    @Query("UPDATE User SET password=:new_password WHERE uid = :id")
+    suspend fun updatePassword(new_password: String?, id: Int)
+
+    @Update
+    suspend fun updateUser(user: User)
 
     @Insert
     suspend fun insertVarargUser(vararg users: User)
--- app/src/main/java/com/intive/patronage22/intivi/database/UserRepository.kt
+++ app/src/main/java/com/intive/patronage22/intivi/database/UserRepository.kt
@@ -3,10 +3,11 @@ package com.intive.patronage22.intivi.database
 import android.content.Context
 import android.util.Log
 import androidx.room.Room
+import kotlinx.coroutines.*
 
 class UserRepository {
 
-    lateinit var dao: UserDao
+    private lateinit var dao: UserDao
 
     fun initialize(applicationContext: Context) {
         dao = Room.databaseBuilder(
@@ -19,24 +20,100 @@ class UserRepository {
         return dao.getUsersList()
     }
 
+    fun findUser(scope: CoroutineScope): List<User>? {
+        var users: List<User>? = null
+        runBlocking{
+            scope.async(Dispatchers.IO){
+                users = dao.getUsersList()
+                return@async users
+            }.await()
+        }
+        return users
+    }
+
     suspend fun findUser(userID: Int): User {
         return dao.findUser(userID)
     }
 
-    suspend fun findUser(username: String, password: String): User {
-        return dao.findUser(username, password)
+    fun findUser(scope: CoroutineScope, userID: Int): User? {
+        var user: User? = null
+        runBlocking{
+            scope.async(Dispatchers.IO){
+                user = dao.findUser(userID)
+                return@async user
+            }.await()
+        }
+        return user
+    }
+
+    suspend fun findUser(email: String, password: String): User {
+        return dao.findUser(email, password)
+    }
+
+    fun findUser(scope: CoroutineScope, email: String, password: String): User? {
+        var user: User? = null
+        runBlocking{
+            scope.async(Dispatchers.IO){
+                user = dao.findUser(email, password)
+                return@async user
+            }.await()
+        }
+        return user
     }
 
     suspend fun findUser(email: String): User {
         return dao.findUser(email)
     }
 
+    fun findUser(scope: CoroutineScope, email: String): User? {
+        var user: User? = null
+        runBlocking{
+            scope.async(Dispatchers.IO){
+                user = dao.findUser(email)
+                return@async user
+            }.await()
+        }
+        return user
+    }
+
+    suspend fun updatePassword(newPassword: String, Id: Int) {
+        return dao.updatePassword(newPassword, Id)
+    }
+
+    fun updatePassword(scope: CoroutineScope,newPassword: String, Id: Int) {
+        scope.launch(Dispatchers.IO){
+            dao.updatePassword(newPassword, Id)
+        }
+    }
+
+    suspend fun updateUser(user: User) {
+        return dao.updateUser(user)
+    }
+
+    fun updateUser(scope: CoroutineScope,user: User) {
+        scope.launch(Dispatchers.IO){
+            dao.updateUser(user)
+        }
+    }
+
     suspend fun insertVarargUser(vararg user: User) {
         return dao.insertVarargUser(*user)
     }
 
+    fun insertVarargUser(scope: CoroutineScope, vararg user: User) {
+        scope.launch(Dispatchers.IO){
+            dao.insertVarargUser(*user)
+        }
+    }
+
     suspend fun deleteUser(user: User) {
         return dao.deleteUser(user)
+    }
+
+    fun deleteUser(scope: CoroutineScope, user: User) {
+        scope.launch(Dispatchers.IO){
+            dao.deleteUser(user)
+        }
     }
 }
 
