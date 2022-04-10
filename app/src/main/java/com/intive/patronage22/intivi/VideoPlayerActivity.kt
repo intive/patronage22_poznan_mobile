0631c48e74957b92aa38ae181b2e384725b4d0fe
Lucas-Fresko <lucas.fresko@gmail.com>
2022-04-11 00:20:17 +0200

    P2022-2133/Exoplayer initial commit.

--- app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
@@ -3,25 +3,70 @@ package com.intive.patronage22.intivi
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.util.Util
+import com.intive.patronage22.intivi.databinding.ActivityVideoPlayerBinding
 
 class VideoPlayerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideoPlayerBinding
+    private var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val controller = WindowInsetsControllerCompat(window, window.decorView)
         controller.hide(WindowInsetsCompat.Type.systemBars())
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        setContentView(R.layout.activity_video_player)
 
-        findViewById<ImageView>(R.id.close_video).setOnClickListener{
+        binding.closeVideo.setOnClickListener{
             finish()
         }
+    }
 
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if ((Util.SDK_INT < 24 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                binding.playerView.player = exoPlayer
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                exoPlayer.setMediaItem(mediaItem)
+            }
+    }
+
+    private fun hideController(){
+        binding.videoControlsRoot.visibility = View.INVISIBLE
+    }
+
+    private fun showController(){
+        binding.videoControlsRoot.visibility = View.VISIBLE
     }
 }
