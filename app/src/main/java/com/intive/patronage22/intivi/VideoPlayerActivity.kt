2ff804fbe257c07b3a3e37cd0f6adbc7b8c2ed97
Lucas-Fresko <lucas.fresko@gmail.com>
2022-04-11 18:28:07 +0200

    P2022-2133/Exoplayer custom controller finalization.

--- app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
@@ -1,5 +1,6 @@
 package com.intive.patronage22.intivi
 
+import android.media.Image
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,29 +14,35 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.get
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.util.Log
 import com.google.android.exoplayer2.util.Util
 import com.intive.patronage22.intivi.databinding.ActivityVideoPlayerBinding
 
 class VideoPlayerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityVideoPlayerBinding
+    private lateinit var controller: WindowInsetsControllerCompat
     private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller = WindowInsetsControllerCompat(window, window.decorView)
         controller.hide(WindowInsetsCompat.Type.systemBars())
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 
-//        binding.closeVideo.setOnClickListener{
-//            finish()
-//        }
+        binding.playerView.findViewById<View>(R.id.close_video_player).setOnClickListener{
+            finish()
+        }
     }
 
     public override fun onStart() {
@@ -52,21 +59,42 @@ class VideoPlayerActivity : AppCompatActivity() {
         }
     }
 
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+        controller.show(WindowInsetsCompat.Type.systemBars())
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.run {
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentMediaItemIndex
+            playWhenReady = this.playWhenReady
+            release()
+        }
+        player = null
+    }
+
     private fun initializePlayer() {
         player = ExoPlayer.Builder(this)
             .build()
             .also { exoPlayer ->
                 binding.playerView.player = exoPlayer
-                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
                 exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentWindow, playbackPosition)
+                exoPlayer.prepare()
             }
     }
-
-//    private fun hideController(){
-//        binding.videoControlsRoot.visibility = View.INVISIBLE
-//    }
-//
-//    private fun showController(){
-//        binding.videoControlsRoot.visibility = View.VISIBLE
-//    }
 }
