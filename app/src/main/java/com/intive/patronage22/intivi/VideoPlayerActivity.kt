471c4c74793ee77eea081d6ba3c8043a34d7f456
Lucas-Fresko <lucas.fresko@gmail.com>
2022-04-12 17:49:32 +0200

    P2022-2133/YouTube API alternative

--- app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
+++ app/src/main/java/com/intive/patronage22/intivi/VideoPlayerActivity.kt
@@ -3,6 +3,7 @@ package com.intive.patronage22.intivi
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
@@ -10,18 +11,49 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.intive.patronage22.intivi.databinding.ActivityVideoPlayerBinding
 
-class VideoPlayerActivity : AppCompatActivity() {
+class VideoPlayerActivity : YouTubeBaseActivity() {
+
+    private lateinit var binding: ActivityVideoPlayerBinding
+    private lateinit var controller: WindowInsetsControllerCompat
+    val api_key =  "AIzaSyAEYViv8-o1hE3p5mbBLwI-bELPFL9UYLc"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val controller = WindowInsetsControllerCompat(window, window.decorView)
         controller.hide(WindowInsetsCompat.Type.systemBars())
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        setContentView(R.layout.activity_video_player)
+        binding = ActivityVideoPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<ImageView>(R.id.close_video).setOnClickListener{
-            finish()
-        }
+        val ytPlayer = binding.ytPlayer
 
+        ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                player?.loadVideo("ndl1W4ltcmg") //Witcher main trailer
+                player?.play()
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Log.d("Exception Log", "Video player Failed")
+            }
+        })
+
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        controller.show(WindowInsetsCompat.Type.systemBars())
     }
 }
